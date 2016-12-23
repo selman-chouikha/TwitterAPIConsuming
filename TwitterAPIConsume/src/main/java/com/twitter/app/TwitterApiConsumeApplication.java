9590431415ce7aa0838a1eb969f4d4eefaec19b7
Selman <Selman@Selman-PC>
2016-12-23 15:02:16 +0100

    Initial commit - Creating Spring boot project

---
+++ TwitterAPIConsume/src/main/java/com/twitter/app/TwitterApiConsumeApplication.java
@@ -0,0 +1,12 @@
+package com.twitter.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterApiConsumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterApiConsumeApplication.class, args);
+	}
+}
